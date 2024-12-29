343d71159cf2feb348525351f1f2fb1bafc6ed65
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2024-12-29 06:31:28 +0000

    add-sub-mul-div

--- programs/mycalculatordapp/src/lib.rs
+++ programs/mycalculatordapp/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("4a2HFaLVkRpduW4bUfGgxPnbWTge88hYvHQkQA8arH1p");
+declare_id!("5ut7bVMyQ97fYovZ8UgafpZputXT6fBZ1fJ6Ahdgbjw4");
 
 #[program]
 pub mod mycalculatordapp {
@@ -9,6 +9,31 @@ pub mod mycalculatordapp {
     pub fn create(ctx: Context<Create>, init_message: String) -> Result<()> {
         let calculator = &mut ctx.accounts.calculator;
         calculator.greeting = init_message;
+        Ok(())
+    }
+
+    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 + num2;
+        Ok(())
+    }
+
+    pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 / num2;
+        calculator.remainder = num1 % num2;
+        Ok(())
+    }
+
+    pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 - num2;
+        Ok(())
+    }
+
+    pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 * num2;
         Ok(())
     }
 }
@@ -20,6 +45,30 @@ pub struct Create<'info>{
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Addition<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+#[derive(Accounts)]
+pub struct Subtraction<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+#[derive(Accounts)]
+pub struct Multiplication<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+#[derive(Accounts)]
+pub struct Division<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
 }
 
 #[account]
--- tests/mycalculatordapp.ts
+++ tests/mycalculatordapp.ts
@@ -25,4 +25,58 @@ describe('mycalculatordapp', () => {
         assert.strictEqual(account.greeting, "Welcome to Solana", "Greeting mismatch");
         // assert.strictEqual(account.user.toBase58(), provider.wallet.publicKey.toBase58(), "User public key mismatch");
     });
+
+    it('Add two numbers', async () => {
+        await program.methods
+            .add(new anchor.BN(2), new anchor.BN(3))
+            .accounts({
+                calculator: calculator.publicKey
+            })
+            .rpc();
+
+        const account = await program.account.calculator.fetch(calculator.publicKey);
+        assert.ok(account.result.eq(new anchor.BN(5)));
+        // assert.strictEqual(account.user.toBase58(), provider.wallet.publicKey.toBase58(), "User public key mismatch");
+    });
+
+    it('Subtract two numbers', async () => {
+        await program.methods
+            .subtract(new anchor.BN(9), new anchor.BN(3))
+            .accounts({
+                calculator: calculator.publicKey
+            })
+            .rpc();
+
+        const account = await program.account.calculator.fetch(calculator.publicKey);
+        assert.ok(account.result.eq(new anchor.BN(6)));
+        // assert.strictEqual(account.user.toBase58(), provider.wallet.publicKey.toBase58(), "User public key mismatch");
+    });
+
+    it('Multiply two numbers', async () => {
+        await program.methods
+            .multiply(new anchor.BN(2), new anchor.BN(3))
+            .accounts({
+                calculator: calculator.publicKey
+            })
+            .rpc();
+
+        const account = await program.account.calculator.fetch(calculator.publicKey);
+        assert.ok(account.result.eq(new anchor.BN(6)));
+        // assert.strictEqual(account.user.toBase58(), provider.wallet.publicKey.toBase58(), "User public key mismatch");
+    });
+
+    it('Divide two numbers', async () => {
+        await program.methods
+            .divide(new anchor.BN(6), new anchor.BN(3))
+            .accounts({
+                calculator: calculator.publicKey
+            })
+            .rpc();
+
+        const account = await program.account.calculator.fetch(calculator.publicKey);
+        assert.ok(account.result.eq(new anchor.BN(2)));
+        assert.ok(account.remainder.eq(new anchor.BN(0)));
+        // assert.strictEqual(account.user.toBase58(), provider.wallet.publicKey.toBase58(), "User public key mismatch");
+    });
+
 });
